5b81743adcdfd67a7257aae308586015e6a42d6d
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-05-09 23:22:53 -0400

    feat: Add remove method to bst

--- binary-search-tree/bst.py
+++ binary-search-tree/bst.py
@@ -37,11 +37,47 @@ class BST_Node:
 
         return False
 
+    def find_inorder_successor(self, bst_node):
+        current = bst_node
+        while current.left:
+            current = current.left
+        return current
+    
+    def remove(self, bst_node, element):
+        if not bst_node: 
+            return bst_node  
+    
+        if element < bst_node.value: 
+            bst_node.left = self.remove(bst_node.left, element) 
+        elif(element > bst_node.value): 
+            bst_node.right = self.remove(bst_node.right, element) 
+        else: 
+            if not bst_node.left: 
+                temp = bst_node.right  
+                bst_node = None 
+                return temp
+            
+            if not bst_node.right: 
+                temp = bst_node.left  
+                bst_node = None
+                return temp 
+     
+            temp = self.find_inorder_successor(bst_node.right) 
+    
+            bst_node.value = temp.value 
+            bst_node.right = self.remove(bst_node.right, temp.value) 
+    
+        return bst_node
 
 # Container tree class
 class BST:
     def __init__(self):
         self.root = None
+
+    def preorder(self):
+        if self.root:
+            self.root.preorder()
+        print()
 
     def insert(self, new_val):
         if self.root:
@@ -49,16 +85,17 @@ class BST:
         else:
             self.root = BST_Node(new_val)
 
-    def preorder(self):
-        if self.root:
-            self.root.preorder()
-        print()
-
     def search(self, element):
         if self.root:
             return self.root.search(element)
 
         return False
+
+    def remove(self, element):
+        if self.root:
+            return self.root.remove(self.root, element)
+        
+        return self.root
 
 
 if __name__ == "__main__":
@@ -73,3 +110,5 @@ if __name__ == "__main__":
     print(tree.search(10))
     print(tree.search(6))
     print(tree.search(2))
+    tree.remove(4)
+    tree.preorder()
